a10535457ae455e8957cf02af7a7c455f3125221
sudokim <khs1778@g.skku.edu>
2022-10-31 15:58:56 +0900

    Initial commit

---
+++ converter.test.py
@@ -0,0 +1,27 @@
+import traceback
+from converter import *
+
+
+while True:
+    pc = PostfixConverter()
+
+    try:
+        ops = input("\n\nInput equation (Ctrl+D to stop): ")
+
+        result, code_string = pc.convert(ops)
+
+        print("\n=== Code String ===", code_string, sep='\n')
+        print("\n=== Execution ===")
+        exec(code_string)
+        print("\n=== Result ===", result, sep='\n')
+    
+    except (EOFError, KeyboardInterrupt):
+        print("\nExiting...")
+        break
+
+    except Exception as e:
+        print("\n=== Exception ===")
+        print(traceback.format_exc())
+
+        print("\n=== Incomplete Code String ===")
+        print(pc.code_string)
